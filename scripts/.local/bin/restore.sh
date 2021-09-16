9dbd9917c61563cd72e5b13bc854900d6a5a1ec1
T W <ymir@ulthar.xyz>
2021-09-15 21:28:20 -0700

    General cleanup of my scripts
    1. Homogenised formatting with tabs instead of spaces
    2. Removed unnecessary code and clunkyness from really old scripts
    3. Deleted a whole bunch of unused scripts
    
    I'm also going to remove the sxhkd/ directory and squash dmenu-doc and
    dmenu-video into a sindle dmenu-url script, but that made git confused
    so I'll do that in another commit.

--- scripts/.local/bin/restore.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Restore a tar archive of my home directory
-# Usage: ./restore.sh archive.tar.gz extract_dir
-
-tar --listed-incremental=/dev/null \
-    --exclude-caches \
-    -xvzf \
-    "$1" \
-    "2"
